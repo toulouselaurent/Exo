1b35d407101425f42cf25a42a45cac0c3629dffc
Laurent <toulouselaurent0@gmail.com>
2020-11-12 17:42:35 +0100

    exercice 2.2 props

--- react/chat-app/src/App.js
+++ react/chat-app/src/App.js
@@ -4,7 +4,21 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact 
+      name = "Irma Perry"
+      avatarUrl = "https://randomuser.me/api/portraits/women/32.jpg"
+      online = { false }
+      />
+      <Contact 
+      name = "Mildred Gomez"
+      avatarUrl = "https://randomuser.me/api/portraits/women/78.jpg"
+      online = { true }
+      />
+      <Contact 
+      name = "Victoria Perkins"
+      avatarUrl = "https://randomuser.me/api/portraits/women/64.jpg"
+      online = { true }
+      />
     </div>
   );
 }
--- react/chat-app/src/components/Contact.js
+++ react/chat-app/src/components/Contact.js
@@ -1,34 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Contact.css';
 
-const user = {
-    name:"Daisy Hopkins",
-    avatarUrl:"https://randomuser.me/api/portraits/women/10.jpg",
-    online: true,
-    isLogged:function(){
-        if(this.online){
-        this.online = true;
-            return <div><div className="status-online"></div><p className="status-text">Online</p></div>;   
-        }else{
-            return <div><div className="status-offline"></div><p className="status-text">Offline</p></div>;
-        }
-    }
-};
+// destructurer le props -> ({name avatar etc ...
 
-function Contact() {
+function Contact(props) {
     return (
     <div className="Contact">
-        <img className="avatar" src={user.avatarUrl} alt=""/>
+        <img className="avatar" src={props.avatarUrl} alt={props.name}/>
         <div>
-            <h4 className="name">{user.name}</h4>
+            <h4 className="name">{props.name}</h4>
             <div className="status">
-                
-                {user.isLogged()}
-                
+
+{/* condition vrai ? true :  false */}
+
+                <div className={ props.online ? "status-online" : "status-offline"}></div>
+                <p className="status-text">{ props.online ? "Online" : "Offline" }</p>          
             </div>
         </div>
     </div>
     );
 }
+
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    avatarUrl: PropTypes.string.isRequired,
+    online: PropTypes.bool.isRequired,
+};
 
 export default Contact;
